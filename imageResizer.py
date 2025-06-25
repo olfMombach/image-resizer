679370523a2a7a02527a4cbc4b1e2ac265af7705
olfMombach (Nils Waldraff) <olf.mombach@gmx.de>
2025-06-25 17:13:41 +0200

    Modularize file

--- imageResizer.py
+++ imageResizer.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from typing import List
 
 
-def process_image(image_path: Path, max_length: int, quality: int):
+def _process_image(image_path: Path, max_length: int, quality: int, verbose: bool = True):
     output_name = Path(f"./resized/{image_path.stem}_resized{image_path.suffix}")
     command = [
         'ffmpeg',
@@ -21,7 +21,22 @@ def process_image(image_path: Path, max_length: int, quality: int):
         stdout=subprocess.DEVNULL,
         stderr=subprocess.DEVNULL,
     )
-    print(f"Finished processing {image_path.name}")
+    if verbose: print(f"Finished processing {image_path.name}")
+
+
+def process_images(images: List[Path], output_folder: Path, max_length: int, quality: int):
+    """Process a list of images (multithreaded). For every image, scale it down using ffmpeg such that any side is not longer than `max_length`, preserving aspect ratio, using ffmpeg quality setting of `quality`, outputting into `output_folder`.
+
+    Args:
+        images (List[Path]): List of image paths to process.
+        output_folder (Path): Folder to output resized images to.
+        max_length (int): Maximum length of any side of the image after resizing.
+        quality (int): Quality setting for ffmpeg, influences resulting file size, higher = worse.
+    """
+
+    # Process images multithreaded
+    with Pool() as pool:
+        pool.starmap(_process_image, zip(images, repeat(max_length), repeat(quality)))
 
 
 def main():
@@ -50,24 +65,23 @@ def main():
         print(f"Ignoring {len(list(root_folder.glob('*'))) - len(images_to_process)} files")
     else:
         images_to_process: List[Path] = args.image
-    
+
     print(f"Found {len(images_to_process)} images")
 
     max_val: int = args.max_length
     quality_val: int = args.quality
 
-    print(f"Resizing images to max length {max_val} with quality {quality_val}")
+    print(f"Resizing images to max length (any side) {max_val} with quality {quality_val}")
 
+    output_folder = Path("./resized")
     try:
-        mkdir(Path("./resized"))
+        mkdir(output_folder)
     except FileExistsError:
         print(f"Please remove or rename existing 'resized' folder to ensure no file loss")
         exit(1)
 
-    # Process images multithreaded
-    with Pool() as pool:
-        pool.starmap(process_image, zip(images_to_process, repeat(max_val), repeat(quality_val)))
-    
+    process_images(images_to_process, output_folder, max_val, quality_val)
+
     print(f"Finished processing {len(images_to_process)} files.")
 
 if __name__ == "__main__":
